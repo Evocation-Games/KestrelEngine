8af25d37804b0e697037cefaa341a2e3a2afab46
Tom Hancocks <tomhancocks@gmail.com>
2020-04-25 16:24:47 +0100

    Add mechanism for loading game scenario data.

--- engine/game/scenario_loader.cpp
+++ engine/game/scenario_loader.cpp
@@ -1,15 +1,15 @@
 // Copyright (c) 2020 Tom Hancocks
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -18,15 +18,40 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include "scripting/lua/lua.hpp"
 #include "game/scenario_loader.hpp"
+#include "libGraphite/rsrc/manager.hpp"
 
-using namespace kestrel;
+// MARK: - Scenario Path
 
-auto main(int argc, const char **argv) -> int
+#if __APPLE__
+auto kestrel::game::default_scenario_path() -> std::string
 {
-    // Do initial setup and configuration for Kestrel.
-    lua::state::global().prepare();
-    game::load_scenario();
-    return 0;
+    return "../Resources/GameCore.ndat";
+}
+#elif __linux__
+auto kestrel::game::default_scenario_path() -> std::string
+{
+    // TODO: Determine what the best location for this is...
+    return "GameCore.ndat";
+}
+#elif defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+#   if defined(_WIN64)
+auto kestrel::game::default_scenario_path() -> std::string
+{
+    // TODO: Determine what the best location for this is...
+    return "GameCore.ndat";
+}
+#   else
+#       error "32-bit Windows is not supported."
+#   endif
+#else
+#   error "Unknown target platform"
+#endif
+
+// MARK: - Loader
+
+auto kestrel::game::load_scenario(std::string path) -> void
+{
+    auto file = std::make_shared<graphite::rsrc::file>(path);
+    graphite::rsrc::manager::shared_manager().import_file(file);
 }
