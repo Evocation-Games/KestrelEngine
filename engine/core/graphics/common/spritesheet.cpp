5cf6c84fceccb66a1334441fe5563883e10b6834
Tom Hancocks <tomhancocks@gmail.com>
2020-06-24 04:22:51 +0100

    Introduce a representation of sprites and spritesheets.

---
+++ engine/core/graphics/common/spritesheet.cpp
@@ -0,0 +1,110 @@
+// Copyright (c) 2020 Tom Hancocks
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "core/graphics/common/spritesheet.hpp"
+
+// MARK: - Sprite Construction
+
+graphics::spritesheet::sprite::sprite(const double& x, const double& y, const double& width, const double& height)
+    : m_frame(x, y, width, height)
+{
+
+}
+
+graphics::spritesheet::sprite::sprite(const math::point& origin, const math::size& size)
+    : m_frame(origin, size)
+{
+
+}
+
+graphics::spritesheet::sprite::sprite(const math::rect& frame)
+    : m_frame(frame)
+{
+
+}
+
+// MARK: - Sprite Accessor
+
+auto graphics::spritesheet::sprite::frame() const -> math::rect
+{
+    return m_frame;
+}
+
+auto graphics::spritesheet::sprite::point() const -> math::point
+{
+    return m_frame.origin;
+}
+
+auto graphics::spritesheet::sprite::size() const -> math::size
+{
+    return m_frame.size;
+}
+
+// MARK: - Spritesheet Construction
+
+graphics::spritesheet::spritesheet(std::shared_ptr<graphics::texture> tex, const double& sprite_width, const double& sprite_height)
+    : m_backing_texture(std::move(tex)), m_sprite_base_size(sprite_width, sprite_height)
+{
+
+}
+
+graphics::spritesheet::spritesheet(std::shared_ptr<graphics::texture> tex, const math::size& sprite_size)
+    : m_backing_texture(std::move(tex)), m_sprite_base_size(sprite_size)
+{
+
+}
+
+// MARK: - Spritesheet Accessors
+
+auto graphics::spritesheet::texture() const -> std::shared_ptr<graphics::texture>
+{
+    return m_backing_texture;
+}
+
+auto graphics::spritesheet::sprite_count() const -> int
+{
+    return m_sprites.size();
+}
+
+auto graphics::spritesheet::at(const int& n) const -> spritesheet::sprite
+{
+    return m_sprites.at(n);
+}
+
+// MARK: - Spritesheet Layout
+
+auto graphics::spritesheet::layout_sprites() -> void
+{
+    // Each sprite is providing the UV texture mapping information that will be used for drawing
+    // the appropriate portion of a texture.
+    auto sprite_uv_width = m_sprite_base_size.width / m_backing_texture->size().width;
+    auto sprite_uv_height = m_sprite_base_size.height / m_backing_texture->size().height;
+
+    // Determine the number of sprites in the grid.
+    auto count_x = static_cast<int>(m_backing_texture->size().width / m_sprite_base_size.width);
+    auto count_y = static_cast<int>(m_backing_texture->size().height / m_sprite_base_size.height);
+
+    for (auto y = 0; y < count_y; ++y) {
+        for (auto x = 0; x < count_x; ++x) {
+            spritesheet::sprite sprite(x * sprite_uv_width, y * sprite_uv_height, sprite_uv_width, sprite_uv_height);
+            m_sprites.emplace_back(sprite);
+        }
+    }
+}
---
+++ engine/core/graphics/common/spritesheet.hpp
@@ -0,0 +1,70 @@
+// Copyright (c) 2020 Tom Hancocks
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#if !defined(KESTREL_SPRITESHEET_HPP)
+#define KESTREL_SPRITESHEET_HPP
+
+#include <memory>
+#include <vector>
+#include "math/rect.hpp"
+#include "core/graphics/common/texture.hpp"
+
+namespace graphics
+{
+
+    class spritesheet: public std::enable_shared_from_this<graphics::spritesheet>
+    {
+    public:
+        struct sprite
+        {
+        private:
+            math::rect m_frame;
+
+        public:
+            sprite(const double& x, const double& y, const double& width, const double& height);
+            sprite(const math::point& origin, const math::size& size);
+            sprite(const math::rect& frame);
+
+            auto frame() const -> math::rect;
+            auto point() const -> math::point;
+            auto size() const -> math::size;
+
+        };
+
+    private:
+        std::shared_ptr<graphics::texture> m_backing_texture;
+        std::vector<spritesheet::sprite> m_sprites;
+        math::size m_sprite_base_size;
+
+        auto layout_sprites() -> void;
+
+    public:
+        spritesheet(std::shared_ptr<graphics::texture> tex, const double& sprite_width, const double& sprite_height);
+        spritesheet(std::shared_ptr<graphics::texture> tex, const math::size& sprite_size);
+
+        auto texture() const -> std::shared_ptr<graphics::texture>;
+
+        auto sprite_count() const -> int;
+        auto at(const int& n) const -> spritesheet::sprite;
+    };
+
+}
+
+#endif //KESTREL_SPRITESHEET_HPP
