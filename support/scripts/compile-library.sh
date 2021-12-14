3978bef0ff708444f68a20d77bff7974914695b3
Tom Hancocks <tomhancocks@gmail.com>
2021-12-14 14:04:40 +0000

    More fixes (Linux and Windows)

--- support/scripts/compile-library.sh
+++ support/scripts/compile-library.sh
@@ -1,5 +1,8 @@
 echo "Building $3 Library"
-cmake -DCMAKE_BUILD_TYPE=Release -Hsubmodules/$1 -Bcmake-$1-build-release
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DFT_DISABLE_BROTLI=TRUE \
+      -Hsubmodules/$1 \
+      -Bcmake-$1-build-release
 cmake --build cmake-$1-build-release -- -j2
 
 mkdir -p vendor
