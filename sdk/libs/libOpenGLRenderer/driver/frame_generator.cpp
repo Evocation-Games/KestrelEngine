2c0bdfb187dfcf848a456e1947a8feca1342ed64
Tom Hancocks <tomhancocks@gmail.com>
2023-10-30 20:37:25 +0000

    Fix missing header for Windows/Linux

--- sdk/libs/libOpenGLRenderer/driver/frame_generator.cpp
+++ sdk/libs/libOpenGLRenderer/driver/frame_generator.cpp
@@ -18,6 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#include <chrono>
 #include <libOpenGLRenderer/driver/frame_generator.hpp>
 #include <libFoundation/profile/profiler.hpp>
 
